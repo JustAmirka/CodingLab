1d6d2384868e2a21f6fc1863c47291ca7bd533e6
JustAmirka <97117667+JustAmirka@users.noreply.github.com>
2022-05-05 16:54:12 +0600

    db + homepage

--- index.js
+++ index.js
@@ -1,19 +1,113 @@
-const express = require("express");
-const bodyParser = require('body-parser')
-const app = express();
+var express = require("express"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose =
+        require("passport-local-mongoose"),
+    User = require("./models/user");
 const ejs=require('ejs');
-const port = 7000;
-
+const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('public'))
 
+
 app.use("/", require("./home"));
 
+const dbConfig = require('./config/database.config.js');
 
-app.listen(port, () =>
-    console.log(`App listening at http://localhost:${port}`)
-);
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Databse Connected Successfully!!");
+}).catch(err => {
+    console.log('Could not connect to the database', err);
+    process.exit();
+});
 
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(require("express-session")({
+    secret: "Rusty is a dog",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//=====================
+// ROUTES
+//=====================
+
+// Showing home page
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+// Showing secret page
+app.get("/secret", isLoggedIn, function (req, res) {
+    res.render("secret");
+});
+
+// Showing register form
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+// Handling user signup
+app.post("/register", function (req, res) {
+    var phone = req.body.phone
+    var firstName = req.body.firstName
+    var lastName = req.body.lastName
+    var email = req.body.email
+    var password = req.body.password
+    User.register(new User({ email: email, firstName: firstName, lastName: lastName, phone: phone  }),
+        password, function (err, user) {
+            if (err) {
+                console.log(err);
+                return res.render("register");
+            }
+
+            passport.authenticate("local")(
+                req, res, function () {
+                    res.render("secret");
+                });
+        });
+});
+
+//Showing login form
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+//Handling user login
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function (req, res) {
+});
+
+//Handling user logout
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect("/login");
+}
+
+var port = process.env.PORT || 4000;
+app.listen(port, function () {
+    console.log(`App listening at http://localhost:${port}`)});
